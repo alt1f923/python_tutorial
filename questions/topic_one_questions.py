11ddf747b5314758ece470b36ed7e019a131f096
weasel boy <35885610+alt1f923@users.noreply.github.com>
2019-06-24 00:29:09 +1200

    Update topic_one_questions.py

--- questions/topic_one_questions.py
+++ questions/topic_one_questions.py
@@ -1,27 +1,48 @@
+#Question one
 def add(x, y):
     """
     Write a function that takes integers x and y and returns the value of their product (x + y)
     """
 
-def complex(x, y, z):
+#Question two
+def add_division(x, y, z):
     """
     Write a function that takes integers x, y and z and returns the value of x + y over the value of z
     You MUST use the add() function for this question
     You can assume z will never be 0
     """
 
+#Question three
+def fstring(s1, s2, x, y):
+    """
+    Write a function that takes strings s1 and s2 and integers x and y and returns a string containing them in the same format as:
+    s2 + ' ' + s1 + '\n' + str(add(x, y))
+    You must use a fstring for this question 
+    """
+
+#Question four
+def string_concatenation(s1):
+    """
+    Write a function that takes the string s1 and returns the value of s1 + the value of s1 in lowercase + the value of s1 in uppercase
+    e.g. s1 = aB -> aBabAB
+    s1 itself must not be edited
+    """
+
+#Question five
 def word_square(s1):
     """
     Write a function that takes a string s1 and returns the square of it as an integer
     You can assume s1 will always be a valid int e.g. s1 = '32'
     """
 
+#Question six
 def float_to_int(x):
     """
     Write a function that takes an float x and returns an integer that is the lowest whole number. e.g. 4.99999 -> 4
     There is more than one way to do this, use whatever one you are most comfortable with
     """
 
+#Question seven
 def fizzbuzz(n):
     """
     Write a function that will return 1 of 4 values: 
