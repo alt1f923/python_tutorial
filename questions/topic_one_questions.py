94039673e78e50c8477a4216e282c726c484494b
weasel boy <35885610+alt1f923@users.noreply.github.com>
2019-06-24 00:07:53 +1200

    Create topic_one_questions.py

---
+++ questions/topic_one_questions.py
@@ -0,0 +1,28 @@
+def add(x, y):
+    """
+    Write a function that takes integers x and y and returns the value of their product (x + y)
+    """
+
+def complex(x, y, z):
+    """
+    Write a function that takes integers x, y and z and returns the value of x + y over the value of z
+    You MUST use the add() function for this question
+    You can assume z will never be 0
+    """
+
+def word_square(s1):
+    """
+    Write a function that takes a string s1 and returns the square of it as an integer
+    You can assume s1 will always be a valid int e.g. s1 = '32'
+    """
+
+def float_to_int(x):
+    """
+    Write a function that takes an float x and returns an integer that is the lowest whole number. e.g. 4.99999 -> 4
+    There is more than one way to do this, use whatever one you are most comfortable with
+    """
+
+def fizzbuzz(n):
+    """
+    Write a function that will return 1 of 4 values: if n is a multiple of 3 return 'fizz', a multiple of 5 'buzz', both 'fizzbuzz', neither n
+    """
